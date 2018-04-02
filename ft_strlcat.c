07038f9fdbe95b2c5f365c3c9f9a0c248fb33cd3
Quint88 <q.espion@gmail.com>
2018-04-02 15:19:35 +0200

    FUCK STRLCAT

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -2,13 +2,36 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(void)
+size_t			ft_strlcat(char *dst, const char *src, size_t size)
 {
-   char dest[20] = "Une chaine";
-   char src[] = "ceci est une chaine qui ne rentre pas dans dest";
- 
-   
-   printf("%s\n", strncat(dest, src, 19));
- 
-   return 0;
+	size_t	r;
+	size_t	src_len;
+	size_t	dst_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	r = 0;
+	i = 0;
+	if (size <= dst_len)
+		return (src_len + size);
+	while (dst[r] && r < size - 1)
+		r++;
+	while (r < size - 1 && src[i])
+	{
+		dst[r] = src[i];
+		r++;
+		i++;
+	}
+	dst[r] = '\0';
+	return (dst_len + src_len);
+}
+
+int		main()
+{
+	char	tab1[20] = "test";
+	char	tab2[20] = "aussi";
+	printf("%d\n", ft_strlcat(tab1, tab2, 0));
+	printf("%s\n", tab1);
+	return (0);
 }
